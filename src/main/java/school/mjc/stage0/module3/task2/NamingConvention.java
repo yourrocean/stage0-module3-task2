a05687f800e18815518045a64a2eebd99fa9bd79
Ocean <112929789+yourrocean@users.noreply.github.com>
2023-09-15 11:47:05 +0600

    Update NamingConvention.java

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -6,8 +6,8 @@ public class NamingConvention {
         int variable_One = 1;
         int variable_Two = 2;
         int variable_three = 3;
-        final int VARIABLEFOUR = 4;
-        final int VARIABLEFIVE = 5;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
         int VariableSix = 6;
         int variableSeven = 7;
         int VariableEight = 8;
